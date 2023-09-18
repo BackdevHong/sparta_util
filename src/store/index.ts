1885f897447511de63c1354a3ac6f289288aa101
backdevhong <ghddls0704@kakao.com>
2023-09-18 19:24:22 +0900

    middle remove

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
 import timeArray from "./timeArray";
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -8,7 +7,6 @@ const store = configureStore({
   reducer: {
     timeArray: timeArray.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
